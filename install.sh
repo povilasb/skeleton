1a986d081478c4bae1f059aae666444bb280d4db
Povilas Balciunas <povilasb@neurotechnology.com>
2013-08-20 10:10:10 +0300

    Adds skeleton installation script.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function print_help()
+{
+	echo ""
+	echo "This is a skeleton installation script. The skeleton itself \
+should be specified by the parameters. First parameter is action, second \
+is it's argument."
+	echo ""
+	echo "Possible skeletons:"
+	echo "1. lua"
+}
+
+
+function install()
+{
+	cp -r "$1" "$2"
+}
+
+if [ "$1" == "-h" ]; then
+	print_help
+elif [ "$1" == "-i" ]; then
+	install $2 $3
+fi
