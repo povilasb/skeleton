0cf9721b59e6a45d3cc1f315d665d0d869d0dadc
Povilas Balciunas <balciunas90@gmail.com>
2013-10-26 13:49:22 -0500

    Gwt: adds simple gwt project skeleton.

---
+++ gwt/src/com/example/skeleton/client/Main.java
@@ -0,0 +1,15 @@
+package com.example.skeleton.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+
+
+public class Main implements EntryPoint{
+
+	public void onModuleLoad(){
+		Element textDiv = Document.get().getElementById("text");
+		textDiv.setInnerHTML("Hello GWT World!");
+	}
+
+}
