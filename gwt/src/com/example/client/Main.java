9a88b82b647829afec3b7e5426cb2ca49bab0995
Povilas Balciunas <balciunas90@gmail.com>
2014-05-22 08:26:33 +0300

    Refactors code.

--- gwt/src/com/example/client/Main.java
+++ gwt/src/com/example/client/Main.java
@@ -7,6 +7,7 @@ import com.google.gwt.dom.client.Element;
 
 public class Main implements EntryPoint{
 
+	@Override
 	public void onModuleLoad(){
 		Element textDiv = Document.get().getElementById("text");
 		textDiv.setInnerHTML("Hello GWT World!");
